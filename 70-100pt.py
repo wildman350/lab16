b8128d62b52bf3683a432fdfa72e815883ca6bf2
wildman350 <s581776@student.roundrockisd.org>
2014-11-11 15:22:21 -0600

    70 pt done

--- 70-100pt.py
+++ 70-100pt.py
@@ -54,10 +54,17 @@ class myApp(object):
     def key(self,event):
         global player
         if event.char == "w":
-            drawpad.move(player,0,-4)
-        
+            drawpad.move(player,0,-20)
+        if event.char == "a":
+            drawpad.move(player,-20,0)
+        if event.char == "s":
+            drawpad.move(player,0,20)
+        if event.char == "d":
+            drawpad.move(player,20,0)
     def collisionDetect(self,rocket):
         rx1,ry1,rx2,ry2 = drawpad.coords(rocket)
+  
+        
         
 
 app = myApp(root)
